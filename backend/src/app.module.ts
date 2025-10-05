bf08f6d3211dfb30124bbf9e180d50efa8d3ed1d
Alejandro <aletomasperren@gmail.com>
2025-10-05 10:30:01 -0300

    Config enviroments

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      envFilePath: `./env/.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
